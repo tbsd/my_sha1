487cc8009b8f698c91b15fe073e17020c464cf61
tbsd <tbsd>
2018-10-20 02:07:02 +0300

    Add leftrotate()

---
+++ my_sha1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+#include <bitset>
+using namespace std;
+
+uint32_t leftrotate(uint32_t set, unsigned count);
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    cout << "Usage: my_sha1 message" << endl;
+    return 1;
+  }
+
+  // ifstream input( argv[1], std::ios::binary );
+  // vector<uint32_t> message(istreambuf_iterator<char>(input), // 32 || 8 ??
+      // (istreambuf_iterator<char>()));
+
+  return 0;
+}
+
+uint32_t leftrotate(uint32_t set, unsigned count) {
+  unsigned shift = count % 32;
+  return (set << shift) | (set >> (32-shift));
+}
