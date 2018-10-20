47be0574458a4e4be2995fa349f04db18e162658
tbsd <tbsd>
2018-10-20 03:35:45 +0300

    Add sha1()

--- my_sha1.cpp
+++ my_sha1.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 uint32_t leftrotate(uint32_t set, unsigned count);
+bitset<160> sha1 (vector<uint8_t> &message);
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -24,4 +25,17 @@ int main(int argc, char **argv) {
 uint32_t leftrotate(uint32_t set, unsigned count) {
   unsigned shift = count % 32;
   return (set << shift) | (set >> (32-shift));
+}
+
+bitset<160> sha1 (vector<uint8_t> &message) {
+  vector<uint32_t> exp_message;
+  add_extra_bits(&message, &exp_message);
+  add_length(&message, &exp_message);
+  uint32_t A = 0x67452301, B = 0xEFCDAB89, C = 0x98BADCFE, D = 0x10325476, 
+           E = 0xC3D2E1F0;
+  for (auto i = exp_message.begin(); i != exp_message.end(); i += 16) {
+    round(i, i + 16, &A, &B, &C, &D, &E)
+  }
+  return (bitset<160>(A) << 128) | (bitset<160>(B) << 96) |
+    (bitset<160>(C) << 64) | (bitset<160>(D) << 32) | (bitset<160>(E));
 }
