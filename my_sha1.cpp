ca798efd6ca7342eca8fc6dfc5d6aa2344efbf94
tbsd <tbsd>
2018-10-21 01:22:49 +0300

    Change sha1() return value type to void

--- my_sha1.cpp
+++ my_sha1.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 uint32_t leftrotate(uint32_t set, unsigned count);
-vector<uint32_t> sha1 (vector<uint8_t> &message);
+void sha1 (vector<uint8_t> &message);
 string vec_to_str(vector<uint32_t> &vec);
 vector<uint32_t> add_extra_bits(const vector<uint8_t> *message);
 void add_length(uint64_t length, vector<uint32_t> *dest);
@@ -44,12 +44,12 @@ string vec_to_str(vector<uint32_t> &vec) {
   return hex_str.str();
 }
 
-vector<uint32_t> sha1 (vector<uint8_t> &message) {
+void sha1 (vector<uint8_t> &message) {
   vector<uint32_t> exp_message = add_extra_bits(&message);
   add_length(message.size(), &exp_message);
   for (auto i = exp_message.begin(); i != exp_message.end(); i += 16) {
     round(i, i + 17);
   }
-  return {A, B, C, D, E};
+  return;
 }
 
